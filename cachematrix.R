b462ab77d345b0db7af6b8bc3de4d1cba9cb2fef
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-04-06 18:24:47 +0100

    First coding of makeCacheMatrix
    Coded makeCacheMatric based on interpitation of makeVector

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <-NULL
+        set <- function (y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        get <- function(x)
+                setinverse <- function(solve) inverse <<- solve
+        getinverse <- function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+        }
 }
 
 
@@ -13,3 +25,5 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
