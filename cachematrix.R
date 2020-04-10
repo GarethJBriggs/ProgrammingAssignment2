b4f495ba77306c15684a0f694d5e4ff1fded5f43
Gareth Briggs <garethjamesbriggs@gmail.com>
2020-04-10 13:57:56 +0100

    Update cachematrix.R
    Global typo on function

--- cachematrix.R
+++ cachematrix.R
@@ -2,27 +2,27 @@
 ##Gareth James Briggs
 ##March 2020
 
-## These funtions enable the calculation of the inverse of a matrix and the inverse's caching. 
+## These functions enable the calculation of the inverse of a matrix and the inverse's caching. 
 ## The inverse need not be calculated again following the input of the same matrix, it can be  
 ## retrieved from cache.
 
-## makeCacheMatrix funtion creates a matrix object 'x', as well as getters and setters to 
+## makeCacheMatrix function creates a matrix object 'x', as well as getters and setters to 
 ## cache the inverse and update and retrieve 'x, and also initialises the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
-        ##set funtion enables the resetting of both the inverse to null and of 'x' to the value
+        ##set function enables the resetting of both the inverse to null and of 'x' to the value
         ## of 'y', in the parent environment via the use of the deep assighnment operator
         set <- function (y) {
                 x <<- y
                 inverse <<- NULL
         }
-        ## get funtion acquires value of 'x' from the parent environment
+        ## get function acquires value of 'x' from the parent environment
         get <- function() x
-        ## setsolve funtion sets the value of the inverse within the parent environment  
+        ## setsolve function sets the value of the inverse within the parent environment  
         ## via thedeep assighnment operator, from the value of solve 
         setsolve <- function(solve) inverse <<- solve
-        ## getsolve funtion acquires the value of the inverse from the parent evironment
+        ## getsolve function acquires the value of the inverse from the parent evironment
         getsolve <- function() inverse
         ## functions put into a list as named elements so the getters and setters can 
         ## be accessed via subsetting using $ 
